333e56381ab5f9863a455b4379d0204a89a11b76
ZarG0i <rex.satzhan@bk.ru>
2026-01-29 14:30:05 +0500

    Add Practice1

--- Practice1/python-basics/Numbers/2ex.py
+++ Practice1/python-basics/Numbers/2ex.py
@@ -5,3 +5,17 @@ z = -5j
 print(type(x))
 print(type(y))
 print(type(z))
+i = 7+2j
+o = 8j
+p = -11j
+
+print(type(i))
+print(type(o))
+print(type(p))
+q = 8 +4j
+w = 96j
+s = 10j
+
+print(type(q))
+print(type(w))
+print(type(s))
--- Practice1/python-basics/Syntax/3ex.py
+++ Practice1/python-basics/Syntax/3ex.py
@@ -3,3 +3,8 @@ y = "Hello, World!"
 
 print(x)
 print(y)
+w = 48
+u = "Hello, KBTU!"
+
+print(w)
+print(u)
